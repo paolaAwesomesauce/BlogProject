79285eae48aa9cf1a9263b6303ef43135e27ef73
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2014-10-29 11:14:10 -0700

    video 16 require header and footer

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-	<?php
-		//Creates Database
-		require_once(__DIR__ . "/controller/create-db.php");
-	?>
+<?php
+	//looks for header.php
+	require_once(__DIR__ . "/view/header.php");
+	
+	//Creates Database
+	require_once(__DIR__ . "/controller/create-db.php");
+	
+	//Looks for footer.php
+	require_once(__DIR__ . "/view/footer.php");
+?>
