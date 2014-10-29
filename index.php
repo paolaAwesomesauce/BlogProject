2defb7206516b10ffdcc2ee0f9d669055f7c06e6
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2014-10-29 11:01:14 -0700

    header and footer.php created

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Blog Project</title>
-</head>
-<body>
 	<?php
 		//Creates Database
 		require_once(__DIR__ . "/controller/create-db.php");
 	?>
-</body>
-</html>
