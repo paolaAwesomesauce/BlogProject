b6b0fac276031a69de11cb1df3c89e28e16e66c3
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2014-11-03 10:55:55 -0800

    video 18 link for post.php

--- view/navigation.php
+++ view/navigation.php
@@ -1,8 +1,14 @@
-<!-- Links you to post.php -->
+<?php
+//gives you the path to post.phps
+	require_once(__DIR__ . "/../model/config.php");
+
+?>
+
+<!-- creates the link for post.php -->
 <nav>
 	<ul>
 		<li>
-			<a href="../post.php">Blog Post Form</a>
+			<a href="<?php echo $path . "post.php"; ?>">Blog Post Form</a>
 		</li>
 	</ul>
 </nav>
