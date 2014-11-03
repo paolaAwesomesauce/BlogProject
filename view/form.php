ce2b9b3bdfa19e77a83f62c853ba1a2d29af7660
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2014-11-03 11:19:07 -0800

    added code to posts.php for title and posts

--- view/form.php
+++ view/form.php
@@ -1,3 +1,15 @@
-<?php
+<h1>Create Blog Posts<h1>
 
-?>
+<form>
+<!-- Creates div and label for title and a text input for title -->
+	<div>
+		<label for="title"> Title: </label>
+		<input type="text" name="title" />
+	</div>
+	
+	<!-- creates div with lablel dor posts and a textarea for posts -->
+	<div>
+		<label for="post">Post: </label>
+		<textarea name="post"></textarea>
+	</div>
+</form>
