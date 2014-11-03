a2f3fac136da132a81e39d3e4452405b90719e23
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2014-11-03 08:02:46 -0800

    linked to bootstrap

--- view/header.php
+++ view/header.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
 	<title>Blog Project</title>
 </head>
 <body>
