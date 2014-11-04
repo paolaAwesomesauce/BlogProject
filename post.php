1c07065bc66d84dc53906ffa2c94ab6c3e43e847
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2014-11-03 17:09:57 -0800

    added requires onces to post.php

--- post.php
+++ post.php
@@ -2,6 +2,9 @@
 	//looks for header.php
 	require_once(__DIR__ . "/view/header.php");
 
+	//Looks for form.php
+	require_once(__DIR__ . "/view/form.php");
+
 	//Looks for footer.php
 	require_once(__DIR__ . "/view/footer.php");
 	
