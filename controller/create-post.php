4f1525586d396aaa21a05fab6857812f9ac0db52
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2014-11-06 09:53:47 -0800

    added code for variable 21

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,7 +1,14 @@
 <?php
+	// varible filter the input, gets the posts from title and filters it to make sure its a string 
 	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
-	$posts = filter_input(INPUT_POST, "posts", FILTER_SANITIZE_STRING);
 
-	echo "<p></p>"
+	//varible filter the input, gets the posts from post and filters it to make sure its a string 
+	$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
+
+	//echos the variable title
+	echo "<p>Title: $title</p>";
+
+	//echos the variable post
+	echo "<p>Post: $post</p>";
 
 ?>
