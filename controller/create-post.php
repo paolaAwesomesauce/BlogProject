eb41a79554a16eb4edad3ecdf3d7af582d3c290b
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2014-11-11 11:21:02 -0800

    video  23 establishing connection

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,4 +1,10 @@
 <?php
+	//looks for database.php
+	require_once(__DIR__ . "/../model/database.php");
+
+	//establishes connection to dattabase
+	$connection = new mysqli($host, $username, $password, $database);
+
 	// varible filter the input, gets the posts from title and filters it to make sure its a string 
 	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 
@@ -10,5 +16,8 @@
 
 	//echos the variable post
 	echo "<p>Post: $post</p>";
+
+	//closes connection
+	$connection->close();
 
 ?>
