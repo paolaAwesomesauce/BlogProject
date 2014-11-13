bec6df977d5319cbc13810deb9708b5d69fb3ba7
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2014-11-13 11:06:23 -0800

    video 26 making classes

---
+++ model/Database.php
@@ -0,0 +1,12 @@
+<?php
+// we are using a class so we wont be repeating the same code over and over again and so it will be easier to read the code.
+	class Database{
+		//variables that can only be seen in this file.
+		private $connection;
+		private $host;
+		private $username;
+		private $password;
+		private $database;
+	}
+
+?>
