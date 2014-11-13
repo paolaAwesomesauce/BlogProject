76ca8d406e4ba8e386b9c3480684687946541d33
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2014-11-13 11:13:53 -0800

    database comments fixed

--- model/Database.php
+++ model/Database.php
@@ -1,5 +1,7 @@
 <?php
 // we are using a class so we wont be repeating the same code over and over again and so it will be easier to read the code.
+// class are a collection of variables and fucntions
+//makes database as an object
 	class Database{
 		//variables that can only be seen in this file.
 		private $connection;
