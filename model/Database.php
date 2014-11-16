bb10ac6eebb73b7b1e0b790bb4bfecf7d81aaafd
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2014-11-16 11:59:28 -0800

    video 28 added more functions to database class

--- model/Database.php
+++ model/Database.php
@@ -10,13 +10,27 @@
 		private $password;
 		private $database;
 
-//the constructor is called on an object after it has been created
+//the constructor is called on an object after it has been created where you can put code that calls on objects
 //the variables in the braces are global variables
 		public function__construct($host, $username, $password, $database){
 			$this->host = $host;
 			$this->username = $username;
 			$this->password = $password;
 			$this->database = $database;
+		}
+
+//function opens connection
+		public function openConnection(){
+
+		}
+
+//function closes connection
+		public function closeConnection(){
+
+		}
+// after you call object you can and be able to call specificlly on the functions above. won't have to constanly repeat ourselves
+		public function query($string){
+
 		}
 	}
 
