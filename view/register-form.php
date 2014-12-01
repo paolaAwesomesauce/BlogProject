0e5442d6546c0764a8c3b2e811f2306e75fce59c
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2014-12-01 11:01:12 -0800

    comments for video 37

--- view/register-form.php
+++ view/register-form.php
@@ -1,20 +1,24 @@
 <!-- Form created so user can register so they can be able to post, posts -->
 <form>
+<!--label for user to resgister their email -->
 	<div>
 		<label for="email">Email: </label>
 		<input type="text" name="email" />
 	</div>
 
+<!-- label for user to register there user name  -->
 	<div>
 		<label for="username">Username: </label>
 		<input type="text" name="username" />
 	</div>
 
+<!-- label for user to input their password; passwrod appears as dots not as text -->
 	<div>
 		<label for="password">Password: </label>
 		<input type="password" name="password" />
 	</div>
 
+<!-- button to submit the info  -->
 	<div>
 		<button type="submit">Submit</button>
 	</div>
