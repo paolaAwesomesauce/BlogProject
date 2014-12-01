fe04a4f65f2fc1f29a07a16467c6540681c19e00
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2014-12-01 10:57:54 -0800

    Video 37creating a  registration form

---
+++ view/register-form.php
@@ -0,0 +1,21 @@
+<!-- Form created so user can register so they can be able to post, posts -->
+<form>
+	<div>
+		<label for="email">Email: </label>
+		<input type="text" name="email" />
+	</div>
+
+	<div>
+		<label for="username">Username: </label>
+		<input type="text" name="username" />
+	</div>
+
+	<div>
+		<label for="password">Password: </label>
+		<input type="password" name="password" />
+	</div>
+
+	<div>
+		<button type="submit">Submit</button>
+	</div>
+</form>
