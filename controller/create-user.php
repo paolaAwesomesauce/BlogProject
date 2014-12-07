7c62a5f72c47f8f48adf21b898172c04ab6c59e7
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2014-12-07 12:00:42 -0800

    video 42 comments

--- controller/create-user.php
+++ controller/create-user.php
@@ -1,9 +1,15 @@
 <?php
 	require_once(__DIR__ . "/../model/config.php");
 
+	//inputs email to database 
 	$email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+
+	//inputs username to database
 	$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+
+	//inputs password to database
 	$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
 
+	//echo our email username and password
 	echo $email . " - " . $username . " - " . $password;
 ?>
