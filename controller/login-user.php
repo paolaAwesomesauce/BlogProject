3573e15162f91297bc77779945b89e9e4fa69f11
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2014-12-09 11:12:58 -0800

    video 48 checking the passwords for login

--- controller/login-user.php
+++ controller/login-user.php
@@ -8,3 +8,20 @@
 
 	//selects our salt password for our users tabkle where our user login 
 	$query = $_SESSION["connection"]->query("SELECT salt, password FROM users WHERE username = '$username'");
+
+	//gets info and stores it to  $row;
+	//checks if password type is the same as password in database
+	//if password doesnt match wont let user login
+	if($query->num_rows == 1) {
+		$row = $query->fetch_array();
+
+		if($row["password"] === crypt($password, $row["salt"])) {
+			echo "<p> Login Successful!</p>";
+		}
+		else{
+			echo "<p> Invalid username and password</p>";
+		}
+	}
+	else{
+		echo "<p> Invalid username and password</p>";
+	}
