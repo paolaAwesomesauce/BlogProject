21a0f3400a70c68d6ce36a4b213451ad7aa010b3
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2014-12-09 10:53:02 -0800

    video 48 checking passwords for login

--- controller/login-user.php
+++ controller/login-user.php
@@ -1,5 +1,9 @@
 <?php
-	echo "Successfully created user";
+	
+	require_once(__DIR__ . "/../model/config.php");
 
+	$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
 
-?>
+	$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
+
+	$query = $_SESSION["connection"]->query("SELECT salt, password FROM users WHERE username = '$username'");
