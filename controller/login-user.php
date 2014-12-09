d7e76c895ab3645c1532fd7f2b64ab0e1806827f
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2014-12-09 10:55:59 -0800

    video 48

--- controller/login-user.php
+++ controller/login-user.php
@@ -1,9 +1,10 @@
 <?php
-	
+	//looks for config.php
 	require_once(__DIR__ . "/../model/config.php");
 
 	$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
 
 	$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
 
+	//selects our salt password for our users tabkle where our user login 
 	$query = $_SESSION["connection"]->query("SELECT salt, password FROM users WHERE username = '$username'");
