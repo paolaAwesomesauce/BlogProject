857e592e1dc2161ddd0c091a4082b4fc525a5462
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2014-12-11 16:58:52 -0800

    trying to do read more

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+$('article').readmore({
+  afterToggle: function(trigger, element, expanded) {
+    if(! expanded) { // The "Close" link was clicked
+      $('html, body').animate( { scrollTop: element.offset().top }, {duration: 100 } );
+    }
+  }
+});
--- view/footer.php
+++ view/footer.php
@@ -1,6 +1,7 @@
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <script type="text/javascript" src="js/readmore.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
 </body>
 </html>
