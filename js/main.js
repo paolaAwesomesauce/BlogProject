22557d8ee98a961deacc0692cff49b3fcb0d478c
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2014-12-15 10:26:31 -0800

    read more

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
+// code for readmore
+$('.posts').readmore({
+  speed: 75,
+  maxHeight: 500
+});
 
-$('article').readmore({
+$('.posts').readmore({
   afterToggle: function(trigger, element, expanded) {
     if(! expanded) { // The "Close" link was clicked
       $('html, body').animate( { scrollTop: element.offset().top }, {duration: 100 } );
